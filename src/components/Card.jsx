41725f525f899da305677606fd9611862eb26b6e
noejasiel <55604384+noejasiel@users.noreply.github.com>
2021-11-27 14:23:04 -0600

    completas las cards del curso

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 
-const Card_curso = () => (
-  <div className="w-2/3 m-auto bg-img-card1 bg-cover h-full relative rounded-xl">
-    <div class="absolute w-full h-full bg-black opacity-60 rounded-2xl"></div>
-    <div className="p-6 flex flex-col justify-center items-center">
-      <h2 className="m-auto text-white z-10">Conociendo la discapacidad</h2>
-      <p className="mt-6 text-white z-10">
-        Conocer los conceptos básicos sobre la discapacidad y sus tipos.
-      </p>
-      <button className="mt-6 text-white z-10 bg-white text-blue-900 w-1/3 rounded-md">
-        Tomar
-      </button>
+const Card_curso = ({ curso }) => {
+  console.log(curso);
+  return (
+    <div
+      className={`w-2/3 m-auto ${curso.img} bg-cover h-full relative rounded-xl`}
+    >
+      <div class="absolute w-full h-full bg-black opacity-60 rounded-2xl"></div>
+      <div className="h-full p-6 flex flex-col justify-around items-center  ">
+        <h2 className=" text-white z-10">{curso.titulo}</h2>
+        <p className="mt-4 text-white z-10">{curso.descripcion}</p>
+        <button className=" text-white z-10 bg-white text-blue-900 w-1/3 rounded-md mt-4">
+          Tomar
+        </button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Card_curso;
